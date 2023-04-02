6a730da5aae9c5d7ba0bd3174ba9dd911dd3f6eb
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-04-02 08:47:48 -0300

    Create Base Routes

---
+++ app/routes/auth.route.js
@@ -0,0 +1,31 @@
+import verifySignUp from "../middleware/verifySignUp.js";
+import controllerAuth from "../controllers/auth.controller.js";
+
+const auth = app => {
+    app.use((req,res, next) => {
+        res.header(
+            'Access-Control-Allow-Headers',
+            'x-access-token, Origin, Content-Type, Accept'
+        );
+        next();
+    });
+
+    app.post(
+        '/api/auth/signup',
+        [
+            verifySignUp.checkDuplicateUsername,
+            verifySignUp.checkRolesExisted
+        ],
+        controllerAuth.signup
+    );
+
+    app.post(
+        '/api/auth/signin',
+        controllerAuth.signin
+    );
+
+    app.post(
+        '/api/auth/refreshtoken',
+        controllerAuth.refreshToken
+    );
+}
---
+++ app/routes/user.route.js
@@ -0,0 +1,18 @@
+import authJwt from "../middleware/authJwt.js";
+import controllerUser from "../controllers/user.controller.js";
+
+const user = app => {
+    app.use((req, res, next) => {
+        res.header(
+            'Access-Control-Allow-Headers',
+            'x-access-token, Origin, Content-Type, Accept'
+        );
+        next();
+    });
+
+    app.get('/api/all', controllerUser.allAccess);
+    app.get('/api/staff', controllerUser.staffBoard);
+    app.get('/api/admin', controllerUser.adminBoard);
+};
+
+export default user;
