8505f813da64d35376988f6ff5d84ba34fd58dfd
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-04-07 14:39:10 -0300

    Update templates files

--- app/config/db.template.js
+++ app/config/db.template.js
@@ -3,14 +3,13 @@
 export const configDB = {
     host: 'localhost',
     user: 'root',
-    password: '123456789',
-    dbname: 'testdb',
+    password: '123456',
+    dbname: 'databasename',
     dialect: 'mysql',
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
-    },
-    roles: ['staff', 'admin']
+    }
 }
--- app/config/secret.template.js
+++ app/config/secret.template.js
@@ -1,4 +1,10 @@
 //Rename this file to auth.config.js
 //export const secret = 'colocar-uma-key-boa-aqui-exemplo-abaixo';
 // Used to generate JWT token
-export const configSecret = 'RJwciX9bVerlk8d-aO2XKgLlTGlnQrGP2He1d-iIdKHhSFT_qEkN1LMMvvalOqRTbMKgKivSEn-F0uugMH50abbYudeTEOUing7aQqBGQq_BElrsiOsR6QmDXl_0iitJo6sZNd1PsFOD49CjEFuurMA2zciq6Z4VBTfbpA2FXPqGyaPW1tJ1Nb6A0PUhRltLXnqPF_psjFYqxEDUARD5ivaBHdFLXT5agAf_eGA5qH0uxUktFaGpx8C8P9fQCkNV';
+export const configSecret = 'pY6fXXUgYmoA7DZIzNjnBWiUb0JrOwu8U7HZ5t-Yn_pS-jh1sOh8eDkeeYzGk7DpoH9d-hmv4zwW1t-0WSWQ5fuoRHpBR8C7bmA-vJI6U--biLwY4WLbaoN1KTKgImJ';
+// First user config
+export const configAdminAcount = {
+    user: "admin",
+    name: "Administrator",
+    password: "senha"
+};
