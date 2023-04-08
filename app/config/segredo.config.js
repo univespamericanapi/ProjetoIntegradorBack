3cab4664e5b665573aec0e536c4c88c2a2dc7d9d
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-04-07 21:32:02 -0300

    Bug fix names of files

--- app/config/segredo.config.js
+++
@@ -1,6 +0,0 @@
-export const segredoConfig = 'pY6fXXUgYmoA7DZIzNjnBWiUb0JrOwu8U7HZ5t-Yn_pS-jh1sOh8eDkeeYzGk7DpoH9d-hmv4zwW1t-0WSWQ5fuoRHpBR8C7bmA-vJI6U--biLwY4WLbaoN1KTKgImJ';
-export const contaAdminConfig = {
-    user: "admin",
-    name: "Administrator",
-    password: "JGLGYJNRs1g1pji110"
-};
