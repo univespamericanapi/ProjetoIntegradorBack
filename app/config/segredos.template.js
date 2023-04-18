75179ba465193d230f80d8b1dde6fa4e33e9a8b1
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-04-17 23:52:51 -0300

    Update segredo.template

--- app/config/segredos.template.js
+++ app/config/segredos.template.js
@@ -4,8 +4,8 @@
 export const segredoConfig = 'pY6fXXUgYmoA7DZIzNjnBWiUb0JrOwu8U7HZ5t-Yn_pS-jh1sOh8eDkeeYzGk7DpoH9d-hmv4zwW1t-0WSWQ5fuoRHpBR8C7bmA-vJI6U--biLwY4WLbaoN1KTKgImJ';
 // First user config
 export const contaAdminConfig = {
-    user: "admin",
-    name: "Administrator",
-    password: "senha",
+    login: "admin",
+    nome: "Administrator",
+    senha: "senha",
     cargo: "admin"
 };
