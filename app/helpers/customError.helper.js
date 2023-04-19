ba8af61a2169e127a5a64df54fdb93827150ab2a
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-04-19 10:41:35 -0300

    Created 'CustomError' helper

---
+++ app/helpers/customError.helper.js
@@ -0,0 +1,16 @@
+import dataUtils from "../utils/data.util.js";
+
+export default class CustomError extends Error {
+    constructor(status = 500, ...params) {
+        super(...params);
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, CustomError);
+        }
+
+        this.name = "CustomError";
+
+        this.status = status;
+        this.date = dataUtils.dataParaStringConsole(new Date());
+    }
+};
