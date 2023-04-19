d0253f6aacf37cfc94ae26ec4cb6d5bf35192338
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-04-19 08:28:50 -0300

    Created listas.constant and mensagens.constant

---
+++ app/constants/listas.constant.js
@@ -0,0 +1,20 @@
+export const cargosConstant = [
+    'admin',
+    'staff',
+    'user',
+    'judge',
+];
+
+export const categoriasConstant = [
+    'Anime/Mangá',
+    'Comics',
+    'Cosfun',
+    'Games',
+    'Mirim',
+    'TV/Cinema'
+];
+
+export const concursosConstant = [
+    'Desfile Cosplay',
+    'Circuito Cosplay'
+];
---
+++ app/constants/mensagens.constant.js
@@ -0,0 +1,59 @@
+export const mensagensConstant = {
+    // unauthorized
+    naoAutorizado: 'Falha! Não autorizado!',
+    requeridoAdm: 'Falha! Requerido cargo de Administrador!',
+    requeridoStaff: 'Falha! Requerido cargo de Staff ou de Administrador!',
+
+    // expired
+    tokenExpirou: 'Falha! Não autorizado! Access Token expirou!',
+    refreshTokenExpirou: 'Refresh token expirou. Por favor, faça um novo login!',
+
+    // registered
+    eventoJaCadastrado: 'Falha! Evento já cadastrado!',
+    participanteJaCadastrado: 'Falha! Participante já foi cadastrado!',
+    usuarioJaCadastrado: 'Falha! Usuário já está em uso!',
+
+    // not found
+    cargoNaoEncontrado: 'Falha! Cargo não existe!',
+    usuarioNaoEncontrado: 'Usuário não encontrado.',
+    refreshTokenNaoEncontrado: 'Refresh token não está no banco de dados!',
+    configuracaoNaoEncontrada: 'Nenhuma configuração foi encontrada para esse evento!',
+    eventoNaoEncontrado: 'Evento não encontrado!',
+    concursoNaoEncontrado: 'Nenhum concurso encontrado!',
+    participanteNaoEncontrado: 'Nenhum competidor foi encontrado!',
+    cidadeNaoEncontrada: 'Nenhuma cidade foi encontrada!',
+    categoriaNaoEncontrada: 'Nenhuma categoria foi encontrada!',
+
+    // successfully registered
+    usuarioRegistrado: 'Usuário foi registrado com sucesso!',
+    participacaoRegistrada: 'Cadastro concluído com sucesso!',
+    eventoRegistrado: 'Novo evento criado com sucesso!',
+    notaRegistrada: 'Notas registradas com sucesso!',
+
+    // successfully updated
+    eventoAtualizado: 'Evento atualizado com sucesso!',
+
+    // successfully deleted
+    eventoDeletado: 'Evento deletado com sucesso!',
+
+    // field not sent
+    refreshTokenNaoEnviado: 'Refresh Token é requerido!',
+    participacaoNaoEnviada: 'Não foram enviadas todas as informações!',
+    tokenNaoEnviado: 'Falha! Nenhum token foi fornecido!',
+    eventoNaoEnviado: 'Não foram enviadas todas as informações!',
+    participacaoNaoEnviado: 'Nenhum parâmetro foi passado!',
+    estadoNaoEnviado: 'Nenhum estado fornecido!',
+    cidadeNaoEnviada: 'Nenhuma cidade fornecida!',
+
+    // invalid
+    senhaInvalida: 'Senha inválida!',
+    cpfInvalido: 'CPF inválido!',
+
+    // others
+    limiteAlcancado: 'Limite de participantes no concurso alcançado!',
+    checkinFeito: 'Checkin efetuado com sucesso!',
+    checkinDesfeito: 'Checkin desfeito com sucesso!',
+    eventoInativo: 'Evento inativo, aguarde até abrir as inscrições!',
+    filaEsperaEncerrada: 'Não há mais vagas para inscrição!',
+    vagasEncerradas: 'As vagas para o concurso acabaram, sua inscrição foi registrada na fila de espera!',
+}
