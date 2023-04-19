5970dafac867b5f8dde84a7a6d3903f3386903eb
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-04-19 10:42:11 -0300

    Created 'coresLog' constant

---
+++ app/constants/coresLog.constant.js
@@ -0,0 +1,34 @@
+export const coresLog = {
+    // flags
+    Reset: "\x1b[0m",
+    Bright: "\x1b[1m",
+    Dim: "\x1b[2m",
+    Underscore: "\x1b[4m",
+    Blink: "\x1b[5m",
+    Reverse: "\x1b[7m",
+    Hidden: "\x1b[8m",
+
+    // foreground color
+    FgBlack: "\x1b[30m",
+    FgRed: "\x1b[31m",
+    FgGreen: "\x1b[32m",
+    FgYellow: "\x1b[33m",
+    FgBlue: "\x1b[34m",
+    FgMagenta: "\x1b[35m",
+    FgCyan: "\x1b[36m",
+    FgWhite: "\x1b[37m",
+    FgGray: "\x1b[90m",
+
+    // background color
+    BgBlack: "\x1b[40m",
+    BgRed: "\x1b[41m",
+    BgGreen: "\x1b[42m",
+    BgYellow: "\x1b[43m",
+    BgBlue: "\x1b[44m",
+    BgMagenta: "\x1b[45m",
+    BgCyan: "\x1b[46m",
+    BgWhite: "\x1b[47m",
+    BgGray: "\x1b[100m",
+}
+
+// Example: console.log(`${coresLog.FgRed}${erro.date}: ${erro.message}${coresLog.Reset}`);
