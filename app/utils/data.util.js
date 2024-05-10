bb084170d00ed17a60ec6bdc9b9ccbddfdfc13e3
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-05-10 16:13:31 -0300

    fix: resolvido problema com a data no cadastramento

--- app/utils/data.util.js
+++ app/utils/data.util.js
@@ -5,7 +5,9 @@ const dataParaString = (data) => moment(data).format('DD/MM/YYYY');
 const dataParaStringConsole = (data) =>
 	moment(data).format('YYYY-MM-DD HH:MM:SS');
 
-const stringParaData = (data) => moment(data, 'YYYY-MM-DD').toDate();
+const stringParaData = (data) => {
+	return moment(data, 'DD-MM-YYYY').toDate();
+};
 
 const dataUtils = {
 	dataParaString,
